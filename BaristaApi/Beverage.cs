f1d57fea06d5303be2bf07372074c3c92bcb0089
Isak Kavallin Johansson <79911856+isakKavallin@users.noreply.github.com>
2021-03-02 15:59:55 +0100

    Commit Isak 2

--- BaristaApi/Beverage.cs
+++ BaristaApi/Beverage.cs
@@ -17,13 +17,13 @@ class Espresso : IBeverage
 class Latte : IBeverage
 {
     public Ingrident Espresso = new Ingrident {
-        IngridentName = "Espresso", IngridientAmount = 50;
-    }
+        IngridentName = "Espresso", IngridientAmount = 50
+    };
     public Ingrident Milk = new Ingrident {
-        IngridentName = "Milk", IngridientAmount = 150;
-    }
+        IngridentName = "Milk", IngridientAmount = 150
+    };
     public List<string> Ingredients => new List<string>();
-    public IBeverage AddIngrident(IIngrident ingrient){
+    public IBeverage AddIngrident(Ingrident ingrient){
         Ingredients.Add(ingridient);
         return this;
     }
