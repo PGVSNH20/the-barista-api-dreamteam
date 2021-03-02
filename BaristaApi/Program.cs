ef176f7e26dd0afad85a050b00c1e66a41ed4c60
Isak Kavallin Johansson <79911856+isakKavallin@users.noreply.github.com>
2021-03-02 15:11:20 +0100

    Commit Isak 2

--- BaristaApi/Program.cs
+++ BaristaApi/Program.cs
@@ -6,17 +6,13 @@ namespace BaristaApi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hej");
-            Console.WriteLine("hej på dig du");
-            Console.WriteLine("hej hej på dig //Isak");
-            Console.WriteLine("hallå! /Cicci");
-            /* pseudo-code
+            
             Espresso espresso = new Espresso().AddWater(20).AddBeans(b => b.AmountInG = 5 && b.Sort = CoffeSorts.Robusta).ToBravage();
             //espresso is type of Espresso
 
             Latte latte = new Espresso().AddWater(20).AddBeans(b => b.AmountInG = 7 && b.Sort = CoffeSorts.Robusta).AddMilk().ToBravage();
             //latte is type of Latte
-             */
+             
         }
     }
 }
