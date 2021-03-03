1e89dd788eeb2e430ea04f51d4f3f48fa816519b
Edgar Runnman <edgar.runnman@gmail.com>
2021-03-03 23:51:34 +0100

    commit

--- BaristaApi/CoffeeService/CoffeeRecepes.cs
+++ BaristaApi/CoffeeService/CoffeeRecepes.cs
@@ -7,37 +7,37 @@ namespace BaristaApi.CoffeeService
     {
         public static List<IIngridient> Espresso = new List<IIngridient>()
         {
-            new CoffeeIngridients.Espresso(200)
+            new Espresso(200)
         };
 
         public static List<IIngridient> Americano = new List<IIngridient>()
         {
-            new CoffeeIngridients.Espresso(100),
+            new Espresso(100),
             new Water(100)
         };
 
         public static List<IIngridient> Cappuccino = new List<IIngridient>()
         {
-            new CoffeeIngridients.Espresso(50),
+            new Espresso(50),
             new MilkFoam(100),
             new Milk(50)
         };
 
         public static List<IIngridient> Macchiato = new List<IIngridient>()
         {
-            new CoffeeIngridients.Espresso(50),
+            new Espresso(50),
             new MilkFoam(150),
         };
 
         public static List<IIngridient> Latte = new List<IIngridient>()
         {
-            new CoffeeIngridients.Espresso(50),
+            new Espresso(50),
             new Milk(150),
         };
 
         public static List<IIngridient> Mocha = new List<IIngridient>()
         {
-            new CoffeeIngridients.Espresso(50),
+            new Espresso(50),
             new ChocolateSyrup(50),
             new Milk(150),
         };
