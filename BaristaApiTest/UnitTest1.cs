c046cb97e5a1fbc9510f4a43efa44f4b9cc21e23
Edgar Runnman <edgar.runnman@gmail.com>
2021-03-04 10:38:25 +0100

    commit tests start

---
+++ BaristaApiTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace BaristaApiTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void When_AddingBeans_GridningBeans_()
+        {
+        }
+    }
+}
