c9a152e8dd1841b298e0903996085ce2b6a2212f
Edgar Runnman <edgar.runnman@gmail.com>
2021-03-04 12:58:09 +0100

    commit

--- BaristaApiTest/UnitTest1.cs
+++ BaristaApiTest/UnitTest1.cs
@@ -8,6 +8,12 @@ namespace BaristaApiTest
         [Fact]
         public void When_AddingBeans_GridningBeans_()
         {
+            var beverage = new CoffeeService.CoffeeService()
+                .AddBeans(b => { b.AmountInG = 100; b.Sort = "Robusta"; return b; })
+                .GrindBeans()
+                .AddEspresso(200)
+                .ToBeverege();
+            Assert.IsType<Espresso>(beverage);
         }
     }
 }
