1f10255949ebf6e51a75c50df05f58f6ea039c73
Edgar Runnman <edgar.runnman@gmail.com>
2021-03-04 19:28:00 +0100

    Xunit test check

--- BaristaApi.Test/FluentApiTests.cs
+++ BaristaApi.Test/FluentApiTests.cs
@@ -1,18 +1,23 @@
+using BaristaApi.Beverages;
+using BaristaApi.CoffeeService;
 using Xunit;
 
 namespace BaristaApi.Tests
 {
-    public class FluentApiTests 
+    public class FluentApiTests
     {
         [Fact]
-        public void When_AddingWaterAndBeans_Expect_Espresso(){
-            //Pseudo code
-            /*
-            // Act
-            var beverage = new EspressoMachine().AddWater().AddBeans().ToBeverage();
-            // Assert
+        public void When_AddEspresso_Expect_Espresso()
+        {
+            var beverage = new CoffeeMachine().AddEspresso().ToBeverage();
             Assert.IsType<Espresso>(beverage);
-            */
+        }
+
+        [Fact]
+        public void When_AddEspressoMilk_Expect_Latte()
+        {
+            var beverage = new CoffeeMachine().AddEspresso().AddMilk().ToBeverage();
+            Assert.IsType<Latte>(beverage);
         }
     }
 }
