04b462473ece6da183902ff48292b77068206f21
ELLIESDATOR\cicci <cicci81@hotmail.com>
2021-03-04 14:04:07 +0100

    commit

---
+++ BaristaApi/Beverages/Cappuccino.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaristaApi.Beverages
+{
+    class Cappuccino : Beverage
+    {
+        public Cappuccino(List<string> ingridients) : base(ingridients)
+        {
+
+        }
+    }
+}
