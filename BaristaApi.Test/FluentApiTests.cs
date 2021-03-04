092b94acff9f8849266824b7dc052661abefbc5e
Edgar Runnman <edgar.runnman@gmail.com>
2021-03-04 19:19:59 +0100

    tests for each coffee type

--- BaristaApi.Test/FluentApiTests.cs
+++ BaristaApi.Test/FluentApiTests.cs
@@ -2,17 +2,79 @@ using Xunit;
 
 namespace BaristaApi.Tests
 {
-    public class FluentApiTests 
+    public class FluentApiTests
     {
         [Fact]
-        public void When_AddingWaterAndBeans_Expect_Espresso(){
-            //Pseudo code
-            /*
-            // Act
-            var beverage = new EspressoMachine().AddWater().AddBeans().ToBeverage();
-            // Assert
-            Assert.IsType<Espresso>(beverage);
-            */
+        public void Expect_Espresso()
+        {
+            var beverage = new CoffeeService.CoffeeService()
+                .AddBeans(b => { b.AmountInG = 100; b.Sort = "Robusta"; return b; })
+                .GrindBeans()
+                .AddEspresso(200)
+                .ToBeverege();
+            Assert.IsType<CoffeeTypes.Espresso>(beverage);
+        }
+
+        [Fact]
+        public void Expect_Americano()
+        {
+            var beverage = new CoffeeService.CoffeeService()
+                .AddBeans(b => { b.AmountInG = 50; b.Sort = "Robusta"; return b; })
+                .GrindBeans()
+                .AddEspresso(100)
+                .AddWater(100)
+                .ToBeverege();
+            Assert.IsType<CoffeeTypes.Americano>(beverage);
+        }
+
+        [Fact]
+        public void Expect_Cappuccino()
+        {
+            var beverage = new CoffeeService.CoffeeService()
+                .AddBeans(b => { b.AmountInG = 50; b.Sort = "Robusta"; return b; })
+                .GrindBeans()
+                .AddEspresso(50)
+                .AddMilkFoam(100)
+                .AddMilk(50)
+                .ToBeverege();
+            Assert.IsType<CoffeeTypes.Cappuccino>(beverage);
+        }
+
+        [Fact]
+        public void Expect_Macchiato()
+        {
+            var beverage = new CoffeeService.CoffeeService()
+                .AddBeans(b => { b.AmountInG = 75; b.Sort = "Robusta"; return b; })
+                .GrindBeans()
+                .AddEspresso(50)
+                .AddMilkFoam(150)
+                .ToBeverege();
+            Assert.IsType<CoffeeTypes.Macchiato>(beverage);
+        }
+
+        [Fact]
+        public void Expect_Mocha()
+        {
+            var beverage = new CoffeeService.CoffeeService()
+                .AddBeans(b => { b.AmountInG = 75; b.Sort = "Robusta"; return b; })
+                .GrindBeans()
+                .AddEspresso(50)
+                .AddMilk(150)
+                .AddChocolateSyrup(50)
+                .ToBeverege();
+            Assert.IsType<CoffeeTypes.Mocha>(beverage);
+        }
+
+        [Fact]
+        public void Expect_Latte()
+        {
+            var beverage = new CoffeeService.CoffeeService()
+                .AddBeans(b => { b.AmountInG = 50; b.Sort = "Robusta"; return b; })
+                .GrindBeans()
+                .AddEspresso(50)
+                .AddMilk(150)
+                .ToBeverege();
+            Assert.IsType<CoffeeTypes.Latte>(beverage);
         }
     }
 }
